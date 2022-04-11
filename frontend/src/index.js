64fbbff898c6a3706d8bc8e74fb8c16ad4b5a762
atroian <andrii.troian@corezoid.com>
2022-04-11 20:26:39 +0300

    add product and rating component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
@@ -7,8 +8,17 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App/>);
+root.render(<App />);
 
+/*
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
